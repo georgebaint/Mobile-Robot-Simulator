aedb97d21ee9caba65c6ec86d6e7c5d4a30a5369
Ivan Poliakov <ivan.polyakov.01@gmail.com>
2024-05-22 14:47:50 +0200

    upd

---
+++ controller training/dusting_simulation.py
@@ -0,0 +1,13 @@
+from agent import Agent
+from environment import Environment
+from forward_kinematics import ForwardKinematics
+from controls import Controls
+from settings import *
+
+class DustingSimulation:
+    def __init__(self):
+        #TODO
+        pass
+
+    def evaluate(genotype):
+        
